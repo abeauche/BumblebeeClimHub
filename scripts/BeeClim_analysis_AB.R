604eac18566285efc9b579149576eb8c439fc867
Alex Beauchemin <abeauche@gmail.com>
2026-03-04 15:12:27 -0800

    Created BeeClim Analysis script

---
+++ scripts/BeeClim_analysis_AB.R
@@ -0,0 +1,12 @@
+# ====================================================
+# Script Name: BeeClim_analysis_AB.R
+# Project: BumblebeeClim 2025-26
+# Author: Alex Beauchemin
+# Date Created: 2026-03-04
+# Last Modified: 2026-03-04
+# Description: This script includes the BumblebeeClim analysis.
+# Dependencies: 
+# ====================================================
+
+#### SETUP ----
+# Load required libraries
